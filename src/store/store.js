3aca4c7128e3ebb8c5f35c3370d5ed0e3d3845a6
aegarmus <aegarmus@gmail.com>
2024-11-03 15:35:50 -0300

    feat: useGlobalStore created with couple names

---
+++ src/store/store.js
@@ -0,0 +1,21 @@
+import { defineStore } from 'pinia';
+
+export const GlobalStore = defineStore('global', {
+    store: () => ({
+        husband: {
+            name: 'Alan',
+            lastname: 'García'
+        },
+        wife: {
+            name: 'Mariel',
+            lastname: 'Retamales'
+        }
+    }),
+    getters: {
+        husbandName: (state) => `${state.husband.name}`,
+        husbandFullName: (state) => `${state.husband.name} ${state.husband.lastname}`,
+        wifeName: (state) => `${state.wife.name}`,
+        wifeFullName: (state) => `${state.wife.name} ${state.wife.lastname}`,
+        coupleNames: (state) => `${state.husband.name} & ${state.wife.name}`
+    }
+})
