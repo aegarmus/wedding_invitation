f80faa50d6f0171afb18a135b8ba421d78b398ee
aegarmus <aegarmus@gmail.com>
2024-11-02 23:47:37 -0300

    feat: BackgroundImage component created

---
+++ src/components/BackgroundImage.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    props: {
+        imageUrl: {
+            type: String,
+            required: true,
+        }
+    }
+};
+</script>
+
+<template>
+    <div class="background-image flex-center" :style="{ backgroundImage: `url(${imageUrl})` }">
+        <div class="overlay"></div>
+        <div class="content-overlay">
+            <slot></slot>
+        </div>
+    </div>
+</template>
