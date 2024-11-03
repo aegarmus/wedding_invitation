8dc8c43dd5fad7cc43234b0a754817ad26aa3204
aegarmus <aegarmus@gmail.com>
2024-11-03 04:06:53 -0300

    feat: animation to text and calendar button reposition

--- src/components/SaveTheDate.vue
+++ src/components/SaveTheDate.vue
@@ -48,13 +48,13 @@ export default {
 
 <template>
     <div class="save-the-date text-center">
-        <h1 class="save-the-date__title">{{ save }} <span class="save-the-day__span">{{ the }}</span> {{ dateText }}</h1>
-        <div class="save-the-date__date-container flex-center">
+        <h1 class="save-the-date__title title-appear">{{ save }} <span class="save-the-day__span">{{ the }}</span> {{ dateText }}</h1>
+        <div class="save-the-date__date-container flex-center date-appear">
             <hr class="save-the-date__line">
             <p class="save-the-date__date">{{ formatDate }}</p>
             <hr class="save-the-date__line">
         </div>
-        <h2 class="save-the-date__couple text-left m-left">{{ husband }} <span>&</span> {{ wife }}</h2>
+        <h2 class="save-the-date__couple text-left m-left couple-appear">{{ husband }} <span>&</span> {{ wife }}</h2>
         <CalendarButton 
             :eventDate="date"
             :timiInit="timeInit"
