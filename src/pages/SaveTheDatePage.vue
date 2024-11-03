d83bf35022506ae4d1a5205ca2c9a9291313fb64
aegarmus <aegarmus@gmail.com>
2024-11-02 23:57:44 -0300

    feat: Save the Date page created with components

---
+++ src/pages/SaveTheDatePage.vue
@@ -0,0 +1,37 @@
+<script>
+import BackgroundImage from '../components/BackgroundImage.vue';
+import SaveTheDate from '../components/SaveTheDate.vue';
+
+export default {
+    name: 'SaveTheDatePage',
+    components: {
+        BackgroundImage,
+        SaveTheDate
+    },
+    data() {
+        return {
+            imageUrl: '/images/prueba-imagen.jpg',
+            title: 'Save the Date!',
+            eventDate: '2025-04-25',
+            timeInit: '17:30',
+            timeFinish: '22:00',
+            husband: 'Alan',
+            wife: 'Mariel'
+        }
+    }
+}
+</script>
+
+<template>
+    <BackgroundImage :imageUrl="imageUrl" >
+        <SaveTheDate
+            :title="title"
+            :date="eventDate"
+            :timeInit="timeInit"
+            :timeFinish="timeFinish"
+            :husband="husband"
+            :wife="wife"
+        />
+
+    </BackgroundImage>
+</template>
