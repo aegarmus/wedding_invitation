2c5bb9b91f8a89b1203e5a9af71ecb543c5a64be
aegarmus <aegarmus@gmail.com>
2024-11-19 17:22:08 -0300

    fix: translate problem and image to webp

--- src/pages/SaveTheDatePage.vue
+++ src/pages/SaveTheDatePage.vue
@@ -190,7 +190,7 @@ export default {
         <BackgroundImage :imageUrl="imageUrl">
           <MusicPlayer ref="musicPlayer" />
           <header class="save-the-date text-center">
-              <h1 class="head__title title-appear">SAVE <span class="save-the-day__span">the</span> DATE</h1>
+              <h1 translate="no" class="head__title title-appear">SAVE <span class="save-the-day__span">the</span> DATE</h1>
               <div class="save-the-date__date-container flex-center zoom-appear">
                   <hr class="save-the-date__line">
                   <p class="save-the-date__date">{{ formatDate }}</p>
